669fc73233d581d8e2b40f8958b2bf7956172a44
Hadrien Perier <hadrien.perier@ynov.com>
2021-02-01 18:49:15 +0100

    Debuggage + Verification finished

--- Assets/Scripts/InputValidation.cs
+++ Assets/Scripts/InputValidation.cs
@@ -6,8 +6,22 @@ using UnityEngine.EventSystems;
 
 public class InputValidation : MonoBehaviour, IPointerDownHandler
 {
-    public InputField  textInputField;
+    public InputField  Field;
     public Text textField;
+    public Text popupText;
+    public GameObject textPopup;
+    
+    public GameObject instructions;
+    public GameObject enigme1;
+    public GameObject informationsButton1;
+    public GameObject enigme2;
+    public GameObject informationsButton2;
+    public GameObject enigme3;
+    public GameObject informationsButton3;
+    public GameObject textInputField;
+    public GameObject validateButton;
+    public GameObject timer;
+    public GameObject victory;
 
     // Start is called before the first frame update
     void Start()
@@ -25,16 +39,34 @@ public class InputValidation : MonoBehaviour, IPointerDownHandler
     {
         ValidateField();
     }
+    
+    public void DisableText()
+    { 
+        popupText.enabled = false; 
+    }    
 
     public void ValidateField()
     {
         if (textField.text == "Pyramid Test")
         {
-            Debug.Log("Vous Avez Gagné!");
+            instructions.SetActive(false);
+            enigme1.SetActive(false);
+            informationsButton1.SetActive(false);
+            enigme2.SetActive(false);
+            informationsButton2.SetActive(false);
+            enigme3.SetActive(false);
+            informationsButton3.SetActive(false);
+            textInputField.SetActive(false);
+            validateButton.SetActive(false);
+            timer.SetActive(false);
+            victory.SetActive(true);
         }
+        
         else
         {
-            Debug.Log("Vous Avez Perdu!");
+            popupText.enabled = true; 
+            textPopup.SetActive(true);
+            Invoke("DisableText", 3f);
         }
     }
     
--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -6,8 +6,12 @@ public class StartGame : MonoBehaviour, IPointerDownHandler
     public GameObject logo;
     public GameObject startButton;
     public GameObject introText;
+    public GameObject instructions;
+    public GameObject enigme1;
     public GameObject informationsButton1;
+    public GameObject enigme2;
     public GameObject informationsButton2;
+    public GameObject enigme3;
     public GameObject informationsButton3;
     public GameObject textInputField;
     public GameObject validateButton;
@@ -18,8 +22,12 @@ public class StartGame : MonoBehaviour, IPointerDownHandler
         logo.SetActive(false);
         startButton.SetActive(false);
         introText.SetActive(false);
+        instructions.SetActive(true);
+        enigme1.SetActive(true);
         informationsButton1.SetActive(true);
+        enigme2.SetActive(true);
         informationsButton2.SetActive(true);
+        enigme3.SetActive(true);
         informationsButton3.SetActive(true);
         textInputField.SetActive(true);
         validateButton.SetActive(true);
