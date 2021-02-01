9c0e6895de33de409512209b45ca6fa262a48315
Anthony <mineof13if@gmail.com>
2021-02-01 17:27:38 +0100

    Initial commit

---
+++ Assets/Scripts/OpenPDF.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OpenPDF : MonoBehaviour, IPointerDownHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OpenPDFs() {
+        Application.OpenURL(System.Environment.CurrentDirectory + "/1.pdf");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OpenPDFs();
+    }
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StartGame : MonoBehaviour, IPointerDownHandler
+{
+    public GameObject startButton;
+    public GameObject informationsButton1;
+    public GameObject informationsButton2;
+    public GameObject informationsButton3;
+    
+    public void StartGames()
+    {
+        startButton.SetActive(false);
+        informationsButton1.SetActive(true);
+        informationsButton2.SetActive(true);
+        informationsButton3.SetActive(true);
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        StartGames();
+    }
+}
